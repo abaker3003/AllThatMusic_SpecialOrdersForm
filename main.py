ffd983c1a9799bfa63fe19ab22853b4b08568b93
Ashley Baker <a.baker3003@icloud.com>
2024-01-05 12:50:28 -0700

    importing xlfile.py to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import subprocess
 import datetime
 import generators as gn
+import xlfile as xl
 
 
 window = tk.Tk()
@@ -10,5 +11,7 @@ window.title("Special Order")
 ticket_num = gn.ticketnum()
 label = tk.Label(window, text=ticket_num)
 label.pack()
+
+x = xl.openxl()
 
 window.mainloop()
