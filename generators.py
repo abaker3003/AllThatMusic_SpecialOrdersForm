3efc7d435816865ecb2c99ac6eb925031ae68b6b
Ashley Baker <a.baker3003@icloud.com>
2024-01-05 12:43:15 -0700

    Editted the SO Reference number string

--- generators.py
+++ generators.py
@@ -2,6 +2,6 @@ import datetime
 
 def ticketnum():
     dt = datetime.datetime.now()
-    tckt = "SO" + dt.strftime("%y%m%d") + ["{%03d}"]
+    tckt = "SO" + dt.strftime("%y%m%d") + "{:03d}".format(3)
 
     return tckt
