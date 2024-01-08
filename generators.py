ed364256e021fc65253db036ab2faba34d67d176
Ashley Baker <a.baker3003@icloud.com>
2024-01-08 15:46:33 -0700

    Trying to configure current widgets in main.py added comments to main.py and generators.py of what i need to work on

--- generators.py
+++ generators.py
@@ -1,7 +1,12 @@
 import datetime
 
+# Need to configure this to handle generating reference number when 
+# file contains data from previous days so the last 3 digits does not 
+# reference the total number of rows
+
 def ticketnum(num):
     dt = datetime.datetime.now()
+
     tckt = "SO" + dt.strftime("%y%m%d") + "{:03d}".format(num)
 
     return tckt
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
 import subprocess
 import datetime
 import generators as gn
@@ -6,6 +8,7 @@ import xlfile as xl
 
 xl_file = xl.open_excel_file()
 window = tk.Tk()
+window.geometry("500x500")
 window.title("Special Order")
 
 ticket_num = gn.ticketnum(xl_file.countRows())
@@ -24,20 +27,42 @@ def save_checkbox_value():
     xl_file.writeOnXL(data)
     xl_file.close_file()
 
-checkbox_vars = []
-options = ["AEC", "SUD", "OOP"]  # Replace with your options
+# Trying radio bottons to replace the checkboxex
+'''opts = StringVar(window, "Options")
+opt_values = {"AEC" : "1",
+              "SUD" : "2",
+              "OOP" : "3"}
+for (text, value) in opt_values.items(): 
+    Radiobutton(window, text = text, variable = opts, 
+        value = value).pack(side = TOP, ipady = 5)'''
 
+# Checkbox for Option AEC, SUD, OOP
+checkbox_vars = []
+options = ["AEC", "SUD", "OOP"]  
+
+option_text = Label(window, text="Option")
+option_text.config(font = ("Times New Roman", 14))
+option_text.pack()
+
+pos = 0
 for option in options:
-    varOpt = tk.IntVar(value=1)  # Set initial value if needed
+    varOpt = tk.IntVar(value=1)  
     checkbox_vars.append(varOpt)
     chk = tk.Checkbutton(window, text=option, variable=varOpt)
-    chk.pack()
+    # Trying to position the checkbox
+    chk.pack(padx=pos, pady=1, side=tk.LEFT)
+    pos += 1
 
+# Checkbox for type
 checkbox_types = []
-types = ["CD", "DVD", "LP", "OTHER"]  # Replace with your options
+types = ["CD", "DVD", "LP", "OTHER"]  
+
+type_text = Label(window, text="Type")
+type_text.config(font = ("Times New Roman", 14))
+type_text.pack()
 
 for t in types:
-    varTypes = tk.IntVar(value=1)  # Set initial value if needed
+    varTypes = tk.IntVar(value=1)  
     checkbox_types.append(varTypes)
     chk = tk.Checkbutton(window, text=t, variable=varTypes)
     chk.pack()
