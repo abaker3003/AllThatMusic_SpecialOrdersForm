ce75e8dabfb1e5dd7a1f19e2c129c5d1dbe8d3a9
Ashley Baker <a.baker3003@icloud.com>
2024-01-12 17:10:50 -0700

    Still dealing with issues

--- xlfile.py
+++ xlfile.py
@@ -19,10 +19,11 @@ class ExcelFile:
     def writeOnXL(self, data):
 
         # Change dtype for phone number to string
-        print(data)
 
         phne = data[3]
+        print(phne)
         data[3] = str(phne)[:11]
+        print(data[3])
 
         # Add new row to worksheet
         self.worksheet.append(data)
