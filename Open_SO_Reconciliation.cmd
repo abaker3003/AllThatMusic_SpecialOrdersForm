102bd0c39a31fb1eb7d85020a1cd09f93723bb32
Ashley Baker <a.baker3003@icloud.com>
2024-03-21 15:18:17 -0600

    Added cmd script

---
+++ Open_SO_Reconciliation.cmd
@@ -0,0 +1,10 @@
+pip install py
+pip install re
+pip install datetime
+pip install getpass
+pip install customtkinter
+pip install ttk
+pip install matplotlib
+pip install pandas
+pip install openpylx
+python main.py
